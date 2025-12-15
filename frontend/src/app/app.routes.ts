c2466f3d5148ae00f1fc456e67bd05e0c83deb56
Carlos Azcarraga <tasajos@gmail.com>
2025-12-14 21:41:12 -0400

    update

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { PacientesList } from './pages/pacientes/pacientes-list/pacientes-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Dashboard },
+  { path: 'pacientes', component: PacientesList },
+  { path: '**', redirectTo: '' },
+];
